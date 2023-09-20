0d7b992ee0f9259eba4bb08e41244f6191e91513
Aung <104800264+aungoo3@users.noreply.github.com>
2023-09-20 16:29:36 -0400

    Update the commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { convertToSpacesPipe } from './product/convert-to-spaces.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    convertToSpacesPipe
   ],
   imports: [
     BrowserModule,
